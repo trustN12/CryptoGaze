28f416f9762c44a703dab56b780af0f16b9f9eb5
trustN12 <nbiswas100@outlook.com>
2024-09-11 20:17:45 +0530

    Update logo and add new assets

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+
 const Logo = () => {
   const glowStyle = {
     boxShadow: "0 0 40px rgba(255, 255, 255, 0.7)",
@@ -12,7 +13,7 @@ const Logo = () => {
       className="absolute top-[1.5rem] left-[1.5rem]  w-40 [text-decoration: none]"
     >
       <motion.img
-        src="src/assets/cryptogaze-high-resolution-logo-transparent.png"
+        src="assets/cryptogaze-high-resolution-logo-transparent.png"
         alt="CryptoGaze"
         style={glowStyle}
         className="rounded-xl border-2 border-r-cyan border-l-green border-t-green border-b-cyan p-1"
